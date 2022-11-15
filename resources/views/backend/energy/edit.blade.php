ef0bf173cad3f4ad5f1cf65677d8358d64cef75b
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2022-11-15 10:41:49 +0700

    fix energy

---
+++ resources/views/backend/energy/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('backend.layouts.app')
+@section('title','Edit Jenis Energi')
+@section('content')
+<x-page-form page='edit' route="energy.index" title="Jenis Energi">
+    <form action="{{route('energy.update', $energy->energy_id)}}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group row">
+
+            {{-- Name --}}
+            <x-form-input label="Nama" type="text" required="required" value="{{$energy->name}}" name="nama">
+            </x-form-input>
+
+            {{-- Email --}}
+            <x-form-input label="Satuan" type="text" required="required" value="{{$energy->unit}}" name="satuan">
+            </x-form-input>
+
+        </div>
+        <div class="card-footer text-right border-0">
+            <a class="btn btn-danger mr-3" href="{{ route('energy.index') }}">Batal</a>
+            <x-tabel-button type="submit" color="primary" title="Simpan"></x-tabel-button>
+        </div>
+    </form>
+</x-page-form>
+@endsection
---
+++ resources/views/backend/energy/index.blade.php
@@ -0,0 +1,57 @@
+@extends('backend.layouts.app')
+@section('title','Daftar Jenis Energi')
+@section('content')
+<x-page-index title="Jenis Energi" buttonLabel="Tambah Jenis Energi" routeCreate="energy.create">
+    @if ($energy->IsNotEmpty())
+    <table id="dataTable" class="table table-striped table-borderless responsive nowrap" style="width:100%">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Satuan</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($energy as $key=>$data)
+            <tr>
+                <td>{{$key+1}}</td>
+                <td>{{$data->name}}</td>
+                <td>{{$data->unit}}</td>
+                <td>
+                    <div class="table-actions btn-group">
+                        <a href="{{route('energy.edit',$data->energy_id)}}" class="table-action btn btn-primary mr-2"
+                            data-toggle="tooltip" title="Ubah">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                        <button class="table-action btn btn-danger delete-btn mr-2" data-toggle="tooltip" title="Delete"
+                            value="{{$data->energy_id}}">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+    <div class="align-items-center bg-light p-3 border-secondary rounded">
+        <span class="">Oops!</span><br>
+        <p><i class="fas fa-info-circle"></i> Belum Terdapat Data Energi</p>
+    </div>
+    @endif
+</x-page-index>
+@include('backend.energy.modal')
+@endsection
+
+@push('scripts')
+<script>
+    $(document).on('click', '.delete-btn', function () {
+        var sid = $(this).val();
+        $('#deleteModal').modal('show')
+        $('#delete_id').val(sid)
+        // alert(sid)
+    });
+
+</script>
+@endpush
